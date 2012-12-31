03dbd3947b66142dddb572ec7c2a21073f2396a8
robotictang <tang.tiong.yew@gmail.com>
2012-12-31 15:24:06 +0800

    Commiting make eclipse-project

---
+++ eddiebot/eddiebot_description/_gtest_from_src/CMakeFiles/gtest_main.dir/DependInfo.cmake
@@ -0,0 +1,14 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/usr/src/gtest/src/gtest_main.cc" "/home/paralax2/github/eddiebot/eddiebot/eddiebot_description/_gtest_from_src/CMakeFiles/gtest_main.dir/src/gtest_main.cc.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/paralax2/github/eddiebot/eddiebot/eddiebot_description/_gtest_from_src/CMakeFiles/gtest.dir/DependInfo.cmake"
+  )

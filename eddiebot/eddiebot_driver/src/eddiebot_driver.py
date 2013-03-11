cc4e5aca29a535a17e712d1e9e577f691f056ea1
robotictang <tang.tiong.yew@gmail.com>
2013-03-11 11:21:23 +0800

    Fixed sensor publishing

--- eddiebot/eddiebot_driver/src/eddiebot_driver.py
+++ eddiebot/eddiebot_driver/src/eddiebot_driver.py
@@ -380,9 +380,9 @@ class Eddiebot():
 #    time.sleep(0.5)
 
   def direct_drive(self, velocity_left, velocity_right):
-    print("direct_drive(self, velocity_left, velocity_right)")
-    print("velocity_left: " + str(int(velocity_left)))
-    print("velocity_right: " + str(int(velocity_right)))
+#    print("direct_drive(self, velocity_left, velocity_right)")
+#    print("velocity_left: " + str(int(velocity_left)))
+#    print("velocity_right: " + str(int(velocity_right)))
     # Mask integers to 2 bytes.
     vl = int(velocity_left) & 0xffff
     vr = int(velocity_right) & 0xffff
@@ -582,8 +582,8 @@ class Eddiebot():
 #    print "right: " + str(adc_right)
 #    print "center: " + str(adc_center)
 #    print "battery: " + str(adc_battery)
-    PING_THRESHOLD = 100
-    ADC_THRESHOLD = 900
+    PING_THRESHOLD = 0
+    ADC_THRESHOLD = 800
     
     ping_left_bumps_f = False
     ping_right_bumps_f = False
@@ -597,83 +597,83 @@ class Eddiebot():
     adc_right_bumps_r = False
     adc_center_bumps_r = False
     
-    if(ping_left_f < PING_THRESHOLD):
-        ping_left_bumps_f = True
-    if(ping_left_f < PING_THRESHOLD):
-        ping_right_bumps_f = True
-    if(ping_left_r < PING_THRESHOLD):
-        ping_left_bumps_r = True
-    if(ping_left_r < PING_THRESHOLD):
-        ping_right_bumps_r = True
+#    if(ping_left_f < PING_THRESHOLD):
+#        ping_left_bumps_f = True
+#    if(ping_right_f < PING_THRESHOLD):
+#        ping_right_bumps_f = True
+#    if(ping_left_r < PING_THRESHOLD):
+#        ping_left_bumps_r = True
+#    if(ping_right_r < PING_THRESHOLD):
+#        ping_right_bumps_r = True
     
     if(adc_left_f > ADC_THRESHOLD):
         adc_left_bumps_f = True
-    if(adc_right_f > ADC_THRESHOLD):
+    if(adc_right_f > ADC_THRESHOLD + 100):
         adc_right_bumps_f = True
-    if(adc_center_f > ADC_THRESHOLD):
+    if(adc_center_f > ADC_THRESHOLD + 100):
         adc_center_bumps_f = True
-    if(adc_left_r > ADC_THRESHOLD):
+    if(adc_left_r > ADC_THRESHOLD + 300): # Plus 300 to avoid side sensing during narrow sliding through the obstacle
         adc_left_bumps_r = True
-    if(adc_right_r > ADC_THRESHOLD):
+    if(adc_right_r > ADC_THRESHOLD + 300):
         adc_right_bumps_r = True
-    if(adc_center_r > ADC_THRESHOLD):
-        adc_center_bumps_r = True
+#    if(adc_center_r > ADC_THRESHOLD):
+#        adc_center_bumps_r = True
         
 #    print "Bumps!!!!!!!!!:"
 #    print ping_left_bumps_f or ping_right_bumps_f or adc_left_bumps_f or adc_right_bumps_f or adc_center_bumps_f or ping_left_bumps_r or ping_right_bumps_r or adc_left_bumps_r or adc_right_bumps_r or adc_center_bumps_r 
 #    
-    return (ping_left_bumps_f or ping_right_bumps_f or adc_left_bumps_f or adc_right_bumps_f or adc_center_bumps_f or ping_left_bumps_r or ping_right_bumps_r or adc_left_bumps_r or adc_right_bumps_r or adc_center_bumps_r ,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,#adc_battery,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0)
+    return (ping_left_bumps_f or ping_right_bumps_f or adc_left_bumps_f or adc_right_bumps_f or adc_center_bumps_f or ping_left_bumps_r or ping_right_bumps_r or adc_left_bumps_r or adc_right_bumps_r or adc_center_bumps_r ,#1
+            adc_center_bumps_f,#2 wall
+            adc_left_bumps_r,#3 cliff_left
+            adc_left_bumps_f,#4 cliff_front_left
+            adc_right_bumps_f,#5 cliff_front_right
+            adc_right_bumps_r,#6 cliff_right
+            0,#7
+            0,#8
+            0,#9
+            0,#10
+            0,#11
+            0,#12
+            0,#13
+            0,#14
+            0,#15
+            0,#16
+            0,#17
+            0,#18
+            0,#19
+            0,#20 adc_battery,
+            0,#21
+            0,#22
+            0,#23
+            0,#24
+            0,#25
+            0,#26
+            0,#27
+            0,#28
+            0,#29
+            0,#30
+            0,#31
+            0,#32
+            0,#33
+            0,#34
+            0,#35
+            0,#36
+            0,#37
+            0,#38
+            0,#39 light_bumper
+            0,#40 light_bump_left
+            0,#41 light_bump_front_left
+            0,#42 light_bump_center_left
+            0,#43 light_bump_center_right
+            0,#44 light_bump_front_right
+            0,#45 light_bump_right
+            0,#46
+            0,#47
+            0,#48
+            0,#49
+            0,#50
+            0,#51
+            0)#52
 #   (ping_left, ping_right, adc_left, adc_right, adc_center, adc_battery)
         
         
